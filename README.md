f5db2c24c31674d9703015b0688d8c4f626c554c
Asato Wakisaka <asato.wakisaka@gmail.com>
2015-05-12 16:25:37 +0900

    $ minil new Plack-Middleware-RevisionPlate

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Plack::Middleware::RevisionPlate - It's new $module
+
+# SYNOPSIS
+
+    use Plack::Middleware::RevisionPlate;
+
+# DESCRIPTION
+
+Plack::Middleware::RevisionPlate is ...
+
+# LICENSE
+
+Copyright (C) Asato Wakisaka.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Asato Wakisaka <asato.wakisaka@gmail.com>
---
+++ lib/Plack/Middleware/RevisionPlate.pm
@@ -0,0 +1,39 @@
+package Plack::Middleware::RevisionPlate;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Plack::Middleware::RevisionPlate - It's new $module
+
+=head1 SYNOPSIS
+
+    use Plack::Middleware::RevisionPlate;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::RevisionPlate is ...
+
+=head1 LICENSE
+
+Copyright (C) Asato Wakisaka.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Asato Wakisaka E<lt>asato.wakisaka@gmail.comE<gt>
+
+=cut
+
